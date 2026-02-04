8ec0ac424c2d884ac84438c0bafb1bb9f3865383
emilymcge <emilymcgettrick@gmail.com>
2026-02-04 17:23:47 -0500

    feat(App.jsx): alphabetizeTwoWords

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 //import { useState } from 'react';
 
 import List from './components/List.jsx';
@@ -21,5 +22,27 @@ function App() {
 
 
 
+function alphabetizeTwoWords(word1, word2) {
+  let first = '';
+  let second = '';
 
+  let index = 0;
+  while (index < word1.length) {
+    if (!word2[index]) return [word2, word1];
+
+    if (word1[index] < word2[index]) {
+      return [word1, word2];
+    }
+    if (word1[index] > word2[index]) {
+      return [word2, word1];
+    }
+    index++;
+  }
+  return [word1, word2];
+}
+
+console.log(
+  "alphabetizeTwoWords('emily', 'emili')",
+  alphabetizeTwoWords('emily', 'emili'),
+);
 export default App;
