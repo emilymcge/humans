2e43c0fc5aef5cee775992b3493e8e09e22020c6
emilymcge <emilymcgettrick@gmail.com>
2026-02-11 16:44:01 -0500

    fix(List.jsx): add keys to ListItems

--- server/src/components/List.jsx
+++ server/src/components/List.jsx
@@ -3,8 +3,13 @@ import ListItem from './ListItem.jsx';
 export default function List({ people }) {
   //difference between putting export here vs bottom?
   //console.log('people ', people);
-  let peopleList = people.map((person) => {
-    return <ListItem person={person} />;
+  let peopleList = people.map((person, id) => {
+    return (
+      <ListItem
+        person={person}
+        key={id}
+      />
+    );
   });
   // console.log('peopleList', peopleList);
   return <div className='list'>{peopleList}</div>;
