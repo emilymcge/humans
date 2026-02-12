30863374553dfd558f21c2680cc4f25b004ff458
emilymcge <emilymcgettrick@gmail.com>
2026-02-12 14:32:27 -0500

    feat(Form.jsx):add padding and button

--- server/src/components/Form.jsx
+++ server/src/components/Form.jsx
@@ -1,36 +1,38 @@
-import { Fieldset, Field, Input } from '@chakra-ui/react';
+import { Button, Container, Fieldset, Field, Input } from '@chakra-ui/react';
 
 export default function Form() {
   return (
     <>
-      <Fieldset.Root className='form'>
-        <Fieldset.Legend>New Human</Fieldset.Legend>
-        <Fieldset.HelperText>Enter new human information</Fieldset.HelperText>
-        <Fieldset.Content>
-          <Field.Root>
-            <Field.Label>First Name</Field.Label>
-            <Input
-              name='firstName'
-              type='firstName'
-            />
-          </Field.Root>
-          <Field.Root>
-            <Field.Label>Last Name</Field.Label>
-            <Input
-              name='lastName'
-              type='lastName'
-            />
-          </Field.Root>
-          <Field.Root>
-            <Field.Label>Job</Field.Label>
-            <Input
-              name='job'
-              type='job'
-            />
-          </Field.Root>
-        </Fieldset.Content>
-      </Fieldset.Root>
-      ;{' '}
+      <Container padding='30px'>
+        <Fieldset.Root className='form'>
+          <Fieldset.Legend>New Human</Fieldset.Legend>
+          <Fieldset.HelperText>Enter new human information</Fieldset.HelperText>
+          <Fieldset.Content>
+            <Field.Root>
+              <Field.Label>First Name</Field.Label>
+              <Input
+                name='firstName'
+                type='firstName'
+              />
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>Last Name</Field.Label>
+              <Input
+                name='lastName'
+                type='lastName'
+              />
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>Job</Field.Label>
+              <Input
+                name='job'
+                type='job'
+              />
+            </Field.Root>
+          </Fieldset.Content>
+        </Fieldset.Root>
+        <Button>Click</Button>
+      </Container>
     </>
   );
 }
